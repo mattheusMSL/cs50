2d85f885dd85aadf16f55c808e8514f7b27f996c
Matheus Santos Lopes <matthewpeterparker@gmail.com>
2024-03-12 13:50:10 -0300

    adding new changes

--- week-1/hello.c
+++ week-1/hello.c
@@ -1,5 +1,14 @@
+#include <cs50.h>
 #include <stdio.h>
 
+int add(int a, int b);
+
 int main(void){
-  printf("hello, world\n");
+  // printf("hello, world\n");
+  string answer = get_string("What's your nome?  ");
+  printf("hello, %s\n", answer);
+} 
+
+int add(int a, int b){
+    return a + b;
 }
